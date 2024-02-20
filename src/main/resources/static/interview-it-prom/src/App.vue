c3133fe5bee736b198a27fecf74cdda20891171b
esta <muptseti_esta@mail.ru>
2024-02-21 04:19:53 +0700

    - начал работу над фронтом. (src/main/resources/static/interview-it-prom)

---
+++ src/main/resources/static/interview-it-prom/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <p>something</p>
+  <MainMenu />
+  <DataTable />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import MainMenu from "./components/MainMenu.vue";
+import DataTable from "./components/DataTable.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    MainMenu,
+    DataTable,
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main/resources/static/interview-it-prom/src/components/DataTable.vue
@@ -0,0 +1,36 @@
+<template>
+  <div v-if="headers.length > 0 && cells.length > 0" class="container">
+    <table class="table table-hover table-striped">
+      <thead>
+        <tr>
+          <td v-for="(head, index) in headers" :key="index">{{ head }}</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(row, index) in cells" :key="index">
+          <td v-for="(data, index) in row" :key="index">{{ data }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  <div v-else>
+    <p>Нет данных для отображения!</p>
+  </div>
+</template>
+<script lang="ts">
+export default {
+  data() {
+    // return {
+    //   headers: ["head1", "head2"],
+    //   cells: [
+    //     ["1.1", "1.2"],
+    //     ["2.1", "2.2"],
+    //   ],
+    // };
+    return {
+      headers: [],
+      cells: [],
+    };
+  },
+};
+</script>
